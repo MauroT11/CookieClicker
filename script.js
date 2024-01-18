bf3e759c71db85982f797840fcc1b36dec5d9617
Maurot11 <mauro.co.uk@hotmail.com>
2024-01-18 15:01:59 +0000

    Footer added

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 let counter = document.getElementById('counter')
 let cookies = 0
 
-
-
 setInterval(function () {
     cookies = cookies + 1;
-    console.log(counter)
     console.log("🍪", cookies);
     counter.textContent = cookies
     localStorage.setItem('Cookies', cookies)
@@ -42,14 +39,13 @@ cookieBtn.addEventListener('click', function() {
 
 const grandmaBtn = document.getElementById('grandmaBtn')
 let grandmaOwn = document.getElementById('grandmaOwn')
+let grandmaCounter = 0
 
 grandmaBtn.addEventListener('click', function(event) {
     if (cookies >= 5) {
         cookies = cookies - 5
-        console.log(grandmaOwn)
-        grandmaOwn = grandmaOwn + 1
-        grandmaOwn.textContent = grandmaOwn
-        console.log(grandmaOwn)
+        grandmaCounter = grandmaCounter + 1
+        grandmaOwn.textContent = grandmaCounter
         setInterval(function () {
             cookies = cookies + 1;
             console.log("👵", cookies)
@@ -61,10 +57,14 @@ grandmaBtn.addEventListener('click', function(event) {
 // OVEN UPGRADE ACTIVATION
 
 const ovenBtn = document.getElementById('ovenBtn')
+let ovenOwn = document.getElementById('ovenOwn')
+let ovenCounter = 0
 
 ovenBtn.addEventListener('click', function() {
     if (cookies >= 10) {
         cookies = cookies - 10
+        ovenCounter = ovenCounter + 1
+        ovenOwn.textContent = ovenCounter
         setInterval(function () {
             cookies = cookies + 5;
             console.log("🔥", cookies)
@@ -76,10 +76,14 @@ ovenBtn.addEventListener('click', function() {
 // FARM UPGRADE ACTIVATION
 
 const farmBtn = document.getElementById('farmBtn')
+let farmOwn = document.getElementById('farmOwn')
+let farmCounter = 0
 
 farmBtn.addEventListener('click', function() {
     if (cookies >= 20) {
         cookies = cookies - 20
+        farmCounter = farmCounter + 1
+        farmOwn.textContent = farmCounter
         setInterval(function () {
             cookies = cookies + 10;
             console.log("🌽", cookies)
@@ -91,10 +95,14 @@ farmBtn.addEventListener('click', function() {
 // MINE UPGRADE ACTIVATION
 
 const mineBtn = document.getElementById('mineBtn')
+let mineOwn = document.getElementById('mineOwn')
+let mineCounter = 0
 
 mineBtn.addEventListener('click', function() {
     if (cookies >= 50) {
         cookies = cookies - 50
+        mineCounter = mineCounter + 1
+        mineOwn.textContent = mineCounter
         setInterval(function () {
             cookies = cookies + 20;
             console.log("⛏️", cookies)
@@ -106,10 +114,14 @@ mineBtn.addEventListener('click', function() {
 // BANK UPGRADE ACTIVATION
 
 const bankBtn = document.getElementById('bankBtn')
+let bankOwn = document.getElementById('bankOwn')
+let bankCounter = 0
 
 bankBtn.addEventListener('click', function() {
     if (cookies >= 100) {
         cookies = cookies - 100
+        bankCounter = bankCounter + 1
+        bankOwn.textContent = bankCounter
         setInterval(function () {
             cookies = cookies + 50;
             console.log("🏦", cookies)
