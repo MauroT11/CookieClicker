44b3a2b3664bd25171682d4ddaad601a2277b5f6
Maurot11 <mauro.co.uk@hotmail.com>
2024-01-18 13:06:21 +0000

    counter working

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hello")
+let counter = document.getElementById('counter')
+let cookies = 0
+
+setInterval(function () {
+    cookies = cookies + 1;
+    console.log("🍪", cookies);
+    counter.textContent = cookies
+}, 1000); // 1000 milliseconds == 1 second
+
+function buyGrandma(num) {
+    if (num >= 5) {
+        console.log('grand')
+        setInterval(function () {
+            cookies = cookies + 2;
+            console.log("Grandma", cookies)
+        })
+    }
+}
+
+const grandmaBtn = document.getElementById('grandmaBtn')
+
+grandmaBtn.addEventListener('click', buyGrandma(counter))
